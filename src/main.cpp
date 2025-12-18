e3601da4d3de6db2fa013d24755da5689127aab7
TAQI UL HASSAN <taqihassan011@gmail.com>
2025-12-18 07:02:06 +0500

    Implement parcel delivery system in main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "DeliveryManager.h"
+using namespace std;
+
+int main() {
+    DeliveryManager manager;
+    int choice;
+
+    do {
+        cout << "\n--- Parcel Delivery System ---\n";
+        cout << "1. Add Parcel\n";
+        cout << "2. Update Parcel Status\n";
+        cout << "3. Display All Parcels\n";
+        cout << "4. Search Parcel by ID\n";
+        cout << "5. Delete Parcel\n";
+        cout << "6. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        int id;
+        string sender, receiver, address;
+
+        switch (choice) {
+        case 1:
+            cout << "Parcel ID: ";
+            cin >> id;
+            cin.ignore();
+            cout << "Sender Name: ";
+            getline(cin, sender);
+            cout << "Receiver Name: ";
+            getline(cin, receiver);
+            cout << "Delivery Address: ";
+            getline(cin, address);
+            manager.addParcel(id, sender, receiver, address);
+            break;
+
+        case 2:
+            cout << "Enter Parcel ID: ";
+            cin >> id;
+            manager.updateParcelStatus(id);
+            break;
+
+        case 3:
+            manager.displayParcels();
+            break;
+
+        case 4:
+            cout << "Enter Parcel ID: ";
+            cin >> id;
+            manager.searchParcelByID(id);
+            break;
+
+        case 5:
+            cout << "Enter Parcel ID: ";
+            cin >> id;
+            manager.deleteParcel(id);
+            break;
+
+        case 6:
+            cout << "Exiting system...\n";
+            break;
+
+        default:
+            cout << "Invalid choice.\n";
+        }
+    } while (choice != 6);
+
+    return 0;
+}
