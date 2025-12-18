c672ba1928e5a7e34d57d08f9941bdbc09f6fe0b
TAQI UL HASSAN <taqihassan011@gmail.com>
2025-12-18 07:23:47 +0500

    Refactor main function for DeliveryManager usage

--- src/main.cpp
+++ src/main.cpp
@@ -3,75 +3,37 @@
 using namespace std;
 
 int main() {
-    DeliveryManager manager;
-    int choice;
+    DeliveryManager dm;
+    int choice, id;
+    string s, r, a, status;
 
     do {
-        cout << "\n--- Parcel Delivery System ---\n";
-        cout << "1. Add Parcel\n";
-        cout << "2. Update Parcel Status\n";
-        cout << "3. Display All Parcels\n";
-        cout << "4. Search Parcel by ID\n";
-        cout << "5. Delete Parcel\n";
-
-        cout << "6. View Tracking History\n";
-        cout << "7. Exit\n";
-        
+        cout << "\n1.Add 2.Display 3.Update 4.Delete\n";
+        cout << "5.Track 6.Search Sender 7.Search Status\n";
+        cout << "8.Enqueue 9.Process 10.Show Queue\n";
+        cout << "11.Undo 0.Exit\n";
         cin >> choice;
-
-        int id;
-        string sender, receiver, address;
 
         switch (choice) {
         case 1:
-            cout << "Parcel ID: ";
-            cin >> id;
-            cin.ignore();
-            cout << "Sender Name: ";
-            getline(cin, sender);
-            cout << "Receiver Name: ";
-            getline(cin, receiver);
-            cout << "Delivery Address: ";
-            getline(cin, address);
-            manager.addParcel(id, sender, receiver, address);
+            cin >> id; cin.ignore();
+            getline(cin, s);
+            getline(cin, r);
+            getline(cin, a);
+            dm.addParcel(id, s, r, a);
             break;
-
-        case 2:
-            cout << "Enter Parcel ID: ";
-            cin >> id;
-            manager.updateParcelStatus(id);
-            break;
-
-        case 3:
-            manager.displayParcels();
-            break;
-
-        case 4:
-            cout << "Enter Parcel ID: ";
-            cin >> id;
-            manager.searchParcelByID(id);
-            break;
-
-        case 5:
-            cout << "Enter Parcel ID: ";
-            cin >> id;
-            manager.deleteParcel(id);
-            break;
-
-        case 6:
-            cout << "Enter Parcel ID: ";
-            cin >> id;
-            manager.showTrackingHistory(id);
-            break;
-            
-        case 7:
-            cout << "Exiting system...\n";
-            break;
-
-        default:
-            cout << "Invalid choice.\n";
+        case 2: dm.displayParcels(); break;
+        case 3: cin >> id; dm.updateParcelStatus(id); break;
+        case 4: cin >> id; dm.deleteParcel(id); break;
+        case 5: cin >> id; dm.showTrackingHistory(id); break;
+        case 6: cin >> s; dm.searchBySender(s); break;
+        case 7: cin >> status; dm.searchByStatus(status); break;
+        case 8: cin >> id; dm.enqueueParcel(id); break;
+        case 9: dm.processDelivery(); break;
+        case 10: dm.showQueue(); break;
+        case 11: dm.undoLastAction(); break;
         }
-    } while (choice != 6);
+    } while (choice != 0);
 
     return 0;
 }
